1698972ab2da410eccef13363285aa028b378b5b
Mehul Pathak <pathakkmehul@gmail.com>
2024-10-23 19:20:19 +0530

    added data and navbar

---
+++ src/data/data.tsx
@@ -0,0 +1,8 @@
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+    navbar: [
+        { href: "/", icon: HomeIcon, label: "Home" },
+        { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    ],
+}
