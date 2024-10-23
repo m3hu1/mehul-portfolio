cd5e4861ed0774c7ad9e6735554ab8e58546aea0
Mehul Pathak <pathakkmehul@gmail.com>
2024-10-23 19:23:00 +0530

    added navbar icons and updated data

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
@@ -5,4 +6,40 @@ export const DATA = {
         { href: "/", icon: HomeIcon, label: "Home" },
         { href: "/blog", icon: NotebookIcon, label: "Blog" },
     ],
+    contact: {
+        email: "pathakkmehul@gmail.com",
+        tel: "+91 8081421477",
+        social: {
+          GitHub: {
+            name: "GitHub",
+            url: "https://github.com/m3hu1",
+            icon: Icons.github,
+            navbar: true,
+          },
+          LinkedIn: {
+            name: "LinkedIn",
+            url: "https://linkedin.com/in/pathakmehul",
+            icon: Icons.linkedin,
+            navbar: true,
+          },
+          X: {
+            name: "X",
+            url: "https://x.com/pathakkmehul",
+            icon: Icons.x,
+            navbar: true,
+          },
+        //   LeetCode: {
+        //     name: "Leetcode",
+        //     url: "https://leetcode.com/pathakkmehul",
+        //     icon: Icons.leetcode,
+        //     navbar: false,
+        //   },
+          Email: {
+            name: "Send Email",
+            url: "mailto:pathakkmehul@gmail.com",
+            icon: Icons.email,
+            navbar: false,
+          },
+        },
+      },
 }
